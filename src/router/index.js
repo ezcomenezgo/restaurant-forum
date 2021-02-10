3b6d71a940d69eda20db35b1f1416722eb43fb5e
Claire <s3310507057@gmail.com>
2021-02-10 10:25:55 +0800

    add signup page

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,12 @@ const routes = [
     component: SignIn
   },
   {
+    path: '/signup',
+    name: 'sign-up',
+    // 動態載入，因為通常使用者只會用到signup一次，所以不需要每次都重新載入一次signup檔案
+    component: () => import('../views/SignUp.vue')
+  },
+  {
     path: '*',
     name: 'not-found',
     component: NotFound,
