eb9dea9d5783b717520ed3d887b5e04cc86c4ade
Claire <s3310507057@gmail.com>
2021-02-10 11:42:57 +0800

    add main pages and set related routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// import { component } from 'vue/types/umd'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -16,6 +23,26 @@ const routes = [
     name: 'sign-up',
     // 動態載入，因為通常使用者只會用到signup一次，所以不需要每次都重新載入一次signup檔案
     component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   {
     path: '*',
