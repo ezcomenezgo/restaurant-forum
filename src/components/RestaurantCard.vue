1f1310f157595d178ce36ca3daacdd2b13c8cd7c
Claire <s3310507057@gmail.com>
2021-02-11 21:24:23 +0800

    add index page template and data

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="col-md-6 col-lg-4">
+    <div class="card mb-4">
+      <img
+        class="card-img-top"
+        :src="restaurant.image"
+        alt="Card image cap"
+        width="286px"
+        height="180px"
+      />
+      <div class="card-body">
+        <p class="card-text title-wrap">
+          <a href="#">{{ restaurant.name }}</a>
+        </p>
+        <span class="badge badge-secondary">{{
+          restaurant.Category.name
+        }}</span>
+        <p class="card-text text-truncate">
+          {{ restaurant.description }}
+        </p>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-danger btn-border favorite mr-2">
+          移除最愛
+        </button>
+        <button type="button" class="btn btn-primary btn-border favorite mr-2">
+          加到最愛
+        </button>
+        <button type="button" class="btn btn-danger like mr-2">Unlike</button>
+        <button type="button" class="btn btn-primary like mr-2">Like</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 為甚麼跟Restaurants.vue裡面的自定義屬性取的名字不一樣
+    initialRestaurant: {
+      // 已經先在Restaurants.vue裡面的<RestaurantCard>生成迴圈才帶入自定義屬性的，所以是物件不是陣列
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      restaurant: this.initialRestaurant,
+    };
+  },
+};
+</script>
