e9c776a26d9363a53b42d6f739813647af7c44b6
Claire <s3310507057@gmail.com>
2021-02-12 20:28:14 +0800

    add pagination

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -14,6 +14,14 @@
       />
     </div>
     <!-- restaurantPagination -->
+    <RestaurantPagination
+      v-if="totalPage.length > 1"
+      :current-page="currentPage"
+      :total-page="totalPage"
+      :category-id="categoryId"
+      :previous-page="previousPage"
+      :next-page="nextPage"
+    />
   </div>
 </template>
 
@@ -21,6 +29,7 @@
 import NavTabs from "../components/NavTabs";
 import RestaurantCard from "../components/RestaurantCard";
 import RestaurantsNavPills from "../components/RestaurantsNavPills";
+import RestaurantPagination from "../components/RestaurantPagination";
 
 const dummyData = {
   restaurants: [
@@ -274,6 +283,7 @@ export default {
     NavTabs,
     RestaurantCard,
     RestaurantsNavPills,
+    RestaurantPagination,
   },
   data() {
     return {
