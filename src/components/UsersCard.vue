6b185ff5d8d693c4a1dd9d7451bf9a2e79787503
Claire <s3310507057@gmail.com>
2021-02-16 12:02:19 +0800

    add UsersTop page

---
+++ src/components/UsersCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="col-3">
+    <a href="#">
+      <img :src="user.image" width="140px" height="140px" />
+    </a>
+    <h2>{{ user.name }}</h2>
+    <span class="badge badge-secondary"
+      >追蹤人數：{{ user.Followers.length }}</span
+    >
+    <p class="mt-3">
+      <button
+        v-if="user.isFollowed"
+        @click.prevent.stop="unfollowed"
+        type="button"
+        class="btn btn-danger"
+      >
+        取消追蹤
+      </button>
+      <button
+        v-else
+        @click.prevent.stop="followed"
+        type="button"
+        class="btn btn-primary"
+      >
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialUser: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: this.initialUser,
+    };
+  },
+  methods: {
+    followed() {
+      this.user = {
+        ...this.user,
+        isFollowed: true,
+      };
+    },
+    unfollowed() {
+      this.user = {
+        ...this.user,
+        isFollowed: false,
+      };
+    },
+  },
+};
+</script>
