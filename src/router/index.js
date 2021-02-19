7d9d4ecfa86b6a387bcf0ee86875d76202e0f3f5
Claire <s3310507057@gmail.com>
2021-02-19 16:15:23 +0800

    add AdminRestaurantNew page

--- src/router/index.js
+++ src/router/index.js
@@ -60,6 +60,11 @@ const routes = [
     component: () => import('../views/AdminRestaurants.vue')
   },
   {
+    path: '/admin/restaurant/new',
+    name: 'admin-restaurant-new',
+    component: () => import('../views/AdminRestaurantNew.vue')
+  },
+  {
     path: '/admin/restaurants/:id',
     name: 'admin-restaurant',
     component: () => import('../views/AdminRestaurant.vue')
