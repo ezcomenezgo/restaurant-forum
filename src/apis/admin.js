31beecaec60fcfa9d5243b5d3886cc48a16a75ee
Claire <s3310507057@gmail.com>
2021-02-26 17:23:54 +0800

    finish admin show restaurants list and delete API

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -4,12 +4,19 @@ const getToken = () => localStorage.getItem('token')
 export default {
   categories: {
     get() {
-      return apiHelper.get('/admin/categories', { headers: { Authorization: `Bearer ${getToken()}`}})
+      return apiHelper.get('/admin/categories', { headers: { Authorization: `Bearer ${getToken()}` } })
     }
   },
   restaurants: {
-    create ({ formData }) {
-      return apiHelper.post('/admin/restaurants', formData, { headers: { Authorization: `Bearer ${getToken()}`}})
+    create({ formData }) {
+      return apiHelper.post('/admin/restaurants', formData, { headers: { Authorization: `Bearer ${getToken()}` } })
+    },
+    getRestaurants() {
+      return apiHelper.get('/admin/restaurants', { headers: { Authorization: `Bearer ${getToken()}` } })
+    },
+    // 為何變數用解構賦值便可成功回傳status，但沒有雙花括就會回傳data資料
+    deleteRestaurant({ restaurantId }) {
+      return apiHelper.delete(`/admin/restaurants/${restaurantId}`, { headers: { Authorization: `Bearer ${getToken()}` } })
     }
   }
 }
