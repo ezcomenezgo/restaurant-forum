709e803e71b19b3bbec3d7e4c57f3323d8910c89
Claire <s3310507057@gmail.com>
2021-02-28 10:53:33 +0800

    fix conflict

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -8,7 +8,6 @@ export default {
     }
   },
   restaurants: {
-<<<<<<< HEAD
     create ({ formData }) {
       return apiHelper.post('/admin/restaurants', formData, { headers: { Authorization: `Bearer ${getToken()}`}})
     },
@@ -16,18 +15,13 @@ export default {
       return apiHelper.get(`admin/restaurants/${restaurantId}`, { headers: {Authorization: `Bearer ${getToken()}`}})
     },
     update({ restaurantId, formData }) {
-      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, { headers: { Authorization: `Bearer ${getToken()}`}})
-=======
-    create({ formData }) {
-      return apiHelper.post('/admin/restaurants', formData, { headers: { Authorization: `Bearer ${getToken()}` } })
-    },
+      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, { headers: { Authorization: `Bearer ${getToken()}`}})},
     getRestaurants() {
       return apiHelper.get('/admin/restaurants', { headers: { Authorization: `Bearer ${getToken()}` } })
     },
     // 為何變數用解構賦值便可成功回傳status，但沒有雙花括就會回傳data資料
     deleteRestaurant({ restaurantId }) {
       return apiHelper.delete(`/admin/restaurants/${restaurantId}`, { headers: { Authorization: `Bearer ${getToken()}` } })
->>>>>>> 75fdfe157608794b87a50298674d8444806ee4a0
     }
   }
 }
