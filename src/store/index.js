aa9fc5f90574c194bea1563545b3ccdf6c3e880d
Claire <s3310507057@gmail.com>
2021-02-28 16:34:44 +0800

    set currentUser & isAuthenticated

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: {
+      id: -1,
+      name: '',
+      email: '',
+      image: '',
+      isAdmin: false
+    },
+    isAuthenticated: false
   },
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 將API 取得的currentUer覆蓋掉Vuex state中的currentUser
+        ...currentUser
+      }
+      // 將使用者的登入狀態改為true
+      state.isAuthenticated =true
+    }
   },
   actions: {
   },
