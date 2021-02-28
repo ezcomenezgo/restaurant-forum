505e8256ed432207c85bacc73fc4df2aa860d700
Claire <s3310507057@gmail.com>
2021-02-28 10:42:53 +0800

    finish Admin restaurant editing API

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -10,6 +10,12 @@ export default {
   restaurants: {
     create ({ formData }) {
       return apiHelper.post('/admin/restaurants', formData, { headers: { Authorization: `Bearer ${getToken()}`}})
+    },
+    getDetail({ restaurantId }) {
+      return apiHelper.get(`admin/restaurants/${restaurantId}`, { headers: {Authorization: `Bearer ${getToken()}`}})
+    },
+    update({ restaurantId, formData }) {
+      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, { headers: { Authorization: `Bearer ${getToken()}`}})
     }
   }
 }
--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -3,35 +3,17 @@
     <AdminRestaurantForm
       @after-submit="handleAfterSubmit"
       :initial-restaurant="restaurant"
+      :is-processing="isProcessing"
     />
   </div>
 </template>
 
 <script>
 import AdminRestaurantForm from "./../components/AdminRestaurantForm.vue";
+// STEP 1: 載入 adminAPI 和 Toast
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helper'
 
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Laurence Reynolds",
-    tel: "1-657-067-3756 x9782",
-    address: "187 Kirlin Squares",
-    opening_hours: "08:00",
-    description: "sit est mollitia",
-    image:
-      "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
-    viewCounts: 1,
-    createdAt: "2019-07-30T16:24:55.432Z",
-    updatedAt: "2019-07-30T17:26:43.260Z",
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-07-30T16:24:55.429Z",
-      updatedAt: "2019-07-30T16:24:55.429Z",
-    },
-  },
-};
 
 export default {
   components: {
@@ -49,33 +31,74 @@ export default {
         image: "",
         openingHours: "",
       },
+      isProcessing: false
     };
   },
   created() {
     const { id } = this.$route.params;
     this.fetchRestaurant(id);
   },
+  beforeRouteUpdate(to, from, next) {
+    // 路由改變時重新抓取資料
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
+  },
   methods: {
-    handleAfterSubmit(formData) {
-      // 透過 API 將表單資料送到伺服器
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ": " + value);
+    async handleAfterSubmit(formData) {
+      try {
+        this.isProcessing = true
+        const { data } = await adminAPI.restaurants.update({
+          restaurantId: this.restaurant.id, formData
+        })
+
+        if(data.status !== 'success'){
+          throw new Error(data.message)
+        }
+
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch(error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon:'error',
+          title: '無法更新餐廳，請稍候再試'
+        })
       }
     },
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurantId", restaurantId);
-      const { restaurant } = dummyData;
-      this.restaurant = {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await adminAPI.restaurants.getDetail({ restaurantId })
+
+        // STEP 3: 透過解構賦值將需要的資料取出
+        // console.log('data', data)
+        const {
+          id,
+          name,
+          tel,
+          address,
+          opening_hours: openingHours,
+          description,
+          image,
+          CategoryId: categoryId
+        } = data.restaurant
+        this.restaurant = {
         ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryId: restaurant.CategoryId,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours,
+        id,
+        name,
+        categoryId,
+        tel,
+        address,
+        description,
+        image,
+        openingHours,
       };
+      } catch(error) {
+        //  STEP 5: 錯誤處理
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
     },
   },
 };
